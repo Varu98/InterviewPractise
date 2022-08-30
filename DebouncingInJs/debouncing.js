8788b19f60bb536270b0c03f6b1e5312043548d8
Varu <vardaan.agarwal.98@gmail.com>
2022-08-30 15:41:32 +0530

    practise bind polyfill

---
+++ DebouncingInJs/debouncing.js
@@ -0,0 +1,6 @@
+// Debouncing in JavaScript
+
+let counter = 0;
+const onChangeHandler = () => {
+  console.log("pressed", counter++);
+};
--- bindPolyfill.js
+++ bindPolyfill.js
@@ -1,35 +1,41 @@
 // a polyfill is a piece of code (usually Javascript on the web ) used to provide modern functionality on older browsers that do not natively support it.
 // - MDN
+
 let name = {
-  firstName: "Vardaan",
-  lastName: "Agarwal",
+  firstName: "varu",
+  lastName: "agarwal",
 };
-let printName = function (hometown, state) {
+
+let printName = function (hometown, state, country) {
   console.log(
     this.firstName +
       " " +
       this.lastName +
-      " lives in " +
+      " , " +
       hometown +
-      " state: " +
-      state
+      " in " +
+      state +
+      " country " +
+      country
   );
 };
 
-let printMyName = printName.bind(name, "Greno");
-printMyName("UttarPradesh");
+let printMyName = printName.bind(name, "UP");
+printMyName("India", "hello");
 
 // we need to create a polyfill for the above bind method
 
 // we know that the bind method returns a function which is later invoked to get the required output
-
 Function.prototype.myBind = function (...args) {
-  let obj = this;
+  let func = this;
   let params = args.slice(1);
-  return function (...arg0) {
-    obj.apply(args[0], [...params, ...arg0]);
+  return function (...args0) {
+    console.log(func);
+
+    func.apply(args[0], [...params, ...args0]);
   };
 };
 
-let printMyName2 = printName.myBind(name, "Greno");
-printMyName2("UttarPradesh");
+let printMyName2 = printName.myBind(name, "UP");
+
+printMyName2("India", "Hello");
