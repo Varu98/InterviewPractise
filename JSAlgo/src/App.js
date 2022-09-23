e2d75f3eea01cfc8868cae973686a92ba1ce97a5
Varu <vardaan.agarwal.98@gmail.com>
2022-09-23 22:50:37 +0530

    add: FilterTodo

--- JSAlgo/src/App.js
+++ JSAlgo/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import BrandFilter from "./Components/BrandFilter";
+import FilterTodo from "./Components/FilterTodo";
 
 function App() {
   return (
     <div className="App">
-      <BrandFilter />
+      {/* <BrandFilter /> */}
+      <FilterTodo />
     </div>
   );
 }
